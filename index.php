e9ed5b3e075c68b970fa8c93f2b880b46e5f1c32
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-12-04 11:56:30 +0100

    [UPD] created function for generate randomic password

--- index.php
+++ index.php
@@ -8,7 +8,20 @@ if(isset($_GET['lunghezza'])){
     $special='£$%&/=?!"';
     $numbers='0123456789';
 
-    
+    // unisco le variabili
+    $all =   $upper.$lower.$special.$numbers;
+
+    $password = "";
+
+    // creo la password
+    for($i=0; $i < $_GET['lunghezza']; $i++){
+        // prendo un carattere randomico
+         $random = rand(0, strlen($all)-1);
+
+         $randomoCharacter= substr($all, $random, 1);
+
+         $password .= $randomoCharacter;
+    }
 }
 ?>
 
@@ -17,21 +30,25 @@ if(isset($_GET['lunghezza'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Password</title>
 </head>
 <body>
     <h1>Generatore di password sicure</h1>
 
 <hr>
-
+    <!-- il tag form invia al click del bottone il parametro scelto , all'URL -->
     <form action="">
-        <!-- campo input -->
+        <!-- campo input (il 'name' lunghezza viene letto da php perchè l'ho passato al $_GET) -->
         <input type="number" min="3" max="20" id="lunghezza" name="lunghezza"> 
         <label for="lunghezza">Inserici la lunghezza della password che vuoi generare</label>
 
         <br>
-
+        <!-- il bottone invia i dati di default quando si torva dentro un form,
+         automaticamente diventa: type="submit" -->
         <button>Genera!</button>
     </form>
+
+    <h2>La tua password è: <?php echo $password ?></h2>
+
 </body>
 </html>
